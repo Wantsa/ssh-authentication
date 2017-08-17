1bf25e72a319dafcab868d4872286273544509e4
steveallen05 <sallen@amberstyle.ca>
2017-08-17 19:17:35 +0000

    Adding centos initialization script

---
+++ initialize-centos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd ~
+
+# Clone the repo and move it to the appropriate directory
+
+git clone https://github.com/Wantsa/ssh-authentication.git
+rsync -ac ./ssh-authentication/ ~/.ssh/
+rm -rf ./ssh-authentication
+
+sudo yum install pam_yubico ruby
+
+sudo ~/.ssh/rebase.sh
+
+# Add the Yubikey PAM module configs to ssh logins
+
+sudo sed -i -e '1iauth sufficient pam_yubico.so cue interactive id=33507 key=5VsuxjBpHdR+Xz36GVSlnxQv67U= authfile=/home/ubuntu/.ssh/yubikey_mappings mode=client\n' /etc/pam.d/sshd
+
+# Update the SSH config
+
+sudo perl -i.original -pe 's/^ChallengeResponseAuthentication (yes|no)/ChallengeResponseAuthentication yes\nAuthenticationMethods publickey,keyboard-interactive:pam\n/gm' /etc/ssh/sshd_config
+sudo service ssh reload
+
+
+# Append the cron job to the root user's crontab, creating one if it does not exist:
+
+sudo -S bash -c '\echo "*/5 * * * * /home/$USER/.ssh/rebase.sh >/dev/null 2>&1" >> /var/spool/cron/crontabs/root'
+
+
