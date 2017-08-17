e47f4922130935cccae2b9ba51be6b8c98b9bbe0
steveallen05 <sallen@amberstyle.ca>
2017-08-17 20:20:48 +0000

    Changing yum install parameters

--- initialize-elastic-beanstalk.sh
+++ initialize-elastic-beanstalk.sh
@@ -5,7 +5,7 @@ sudo -u $TARGET_USER cd ~
 echo "Switched to $TARGET_USER"
 
 # Clone the repo and move it to the appropriate directory
-yum install –y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 yum install pam_yubico ruby git -y
 
 TMP_GIT_GARGET=/tmp/ssh-authentication/
