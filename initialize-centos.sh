887f6bb8b8d5d25dde1963a27f447fcb42c1cb08
steveallen05 <sallen@amberstyle.ca>
2017-08-17 20:12:25 +0000

    Updating for elastic beanstalk config

--- initialize-centos.sh
+++ initialize-centos.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
+TARGET_USER=ec2-user
+sudo -u $TARGET_USER cd ~
 
-cd ~
+echo "Switched to $TARGET_USER"
 
 # Clone the repo and move it to the appropriate directory
+yum install –y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install pam_yubico ruby git -y
 
-git clone https://github.com/Wantsa/ssh-authentication.git
-rsync -ac ./ssh-authentication/ ~/.ssh/
-rm -rf ./ssh-authentication
+TMP_GIT_GARGET=/tmp/ssh-authentication/
 
-sudo yum install pam_yubico ruby
+sudo -u $TARGET_USER -H git clone https://github.com/Wantsa/ssh-authentication.git $TMP_GIT_GARGET
+sudo -u $TARGET_USER -H rsync -ac $TMP_GIT_GARGET /home/$TARGET_USER/.ssh/
+sudo -u $TARGET_USER -H rm -rf $TMP_GIT_GARGET
 
-sudo ~/.ssh/rebase.sh
+
+sudo -u $TARGET_USER -H sudo /home/$TARGET_USER/.ssh/rebase.sh
 
 # Add the Yubikey PAM module configs to ssh logins
 
-sudo sed -i -e '1iauth sufficient pam_yubico.so cue interactive id=33507 key=5VsuxjBpHdR+Xz36GVSlnxQv67U= authfile=/home/ubuntu/.ssh/yubikey_mappings mode=client\n' /etc/pam.d/sshd
+sed -i -e '1iauth sufficient pam_yubico.so cue interactive id=33507 key=5VsuxjBpHdR+Xz36GVSlnxQv67U= authfile=/home/$TARGET_USER/.ssh/yubikey_mappings mode=client\n' /etc/pam.d/sshd
 
 # Update the SSH config
 
-sudo perl -i.original -pe 's/^ChallengeResponseAuthentication (yes|no)/ChallengeResponseAuthentication yes\nAuthenticationMethods publickey,keyboard-interactive:pam\n/gm' /etc/ssh/sshd_config
-sudo service ssh reload
+perl -i.original -pe 's/^ChallengeResponseAuthentication (yes|no)/ChallengeResponseAuthentication yes\nAuthenticationMethods publickey,keyboard-interactive:pam\n/gm' /etc/ssh/sshd_config
+service sshd reload
 
 
 # Append the cron job to the root user's crontab, creating one if it does not exist:
 
 command="/home/$USER/.ssh/rebase.sh >/dev/null 2>&1"
 job="*/5 * * * * $command"
-cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
+cat <(fgrep -i -v "$command" <(sudo -u $TARGET_USER -H crontab -l)) <(echo "$job") | sudo -u $TARGET_USER -H crontab -
