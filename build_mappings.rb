7fb754755085cc683b3eba9ebc37596e2e5681b3
stevenallen05 <sallen@amberstyle.ca>
2017-08-17 15:38:40 -0700

    Update build_mappings.rb

--- build_mappings.rb
+++ build_mappings.rb
@@ -2,7 +2,7 @@ require 'yaml'
 
 at_exit do
 	keys = YAML::load_file(File.join(File.dirname(File.expand_path(__FILE__)), 'yubikey_mappings.yml'))
-	user = ENV.fetch("USER", ubuntu)
+	user = ENV.fetch("USER", "ubuntu")
 	mapping_string = "#{user}:#{mappings_to_string(keys)}"
 	abort if mapping_string == read_mappings
 	write_mappings(mapping_string)
