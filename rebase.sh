08704dcc867becf4d1596b463e2b47a878a9af08
steveallen05 <sallen@amberstyle.ca>
2017-08-17 23:26:55 +0000

    Updating rebase

--- rebase.sh
+++ rebase.sh
@@ -21,8 +21,8 @@ if [ $CHANGED_FILES -gt 0 ]; then
     SSH_SERVICE=ssh
   fi
 
-  sudo pkill --signal HUP sshd
-  sudo service $SSH_SERVICE restart
+  sudo service $SSH_SERVICE stop
+  sudo service $SSH_SERVICE start
 else
   logger ssh-authentication "Nope, authorized_keys is cool just the way it is"
 fi
